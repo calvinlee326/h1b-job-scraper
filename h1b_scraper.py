c13fc9fed04405255c661653c057a3d8c5e598c5
calvinlee326 <calvinlee326@gmail.com>
2025-02-03 17:24:08 -0800

    feat: init commit with core scraping functionality

---
+++ h1b_scraper.py
@@ -0,0 +1,50 @@
+import requests # to make HTTP requests
+from bs4 import BeautifulSoup # to parse HTML content
+import pandas as pd # to handle data
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+def scrape_h1b_jobs():
+    url = "https://h1bdata.info/topcompanies.php"
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code != 200:
+        print(f"Failed to retrieve data from {url}")
+        return[]
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    companies = []
+    
+    table = soup.find("table", {"class": "table"})
+    
+    if not table:
+        print("Table not found on the page.")
+        return []
+    
+    # Extract company names from the table
+    for row in table.find_all("tr")[1:]:
+        cols = row.find_all("td")
+        if len(cols) >= 4:
+            company = {
+                'Rank': cols[0].text.strip(),
+                'Company Name': cols[1].text.strip(),
+                'Total H-1B Visas Filling': cols[2].text.strip(),
+                'Average Salary': cols[3].text.strip()
+            }
+            companies.append(company)
+    return companies
+
+    
+    
+            
+if __name__ == "__main__":
+    companies = scrape_h1b_jobs()
+    if companies:
+        df = pd.DataFrame(companies)
+        df.to_csv("h1b_companies.csv", index=False)
+        print(f"{len(df)} counts data saved to h1b_companies.csv")
+    else:
+        print("No data was scraped.")
+
